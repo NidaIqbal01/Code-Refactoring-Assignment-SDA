ea0381e5cbc73f90608b2c5e5a25755837f89e8f
faatimax <faatimabukhari@gmail.com>
2026-04-12 16:51:34 +0500

    PricingStrategy.java

---
+++ new refactored code/OCP Implementation/PricingStrategy.java
@@ -0,0 +1,18 @@
+// OCP: New pricing rules (like discounts) can be added without changing Order class
+    interface PricingStrategy {
+    double calculate(List<Book> books);
+    }
+
+    class StandardPricing implements PricingStrategy 
+    {
+        public double calculate(List<Book> books) {
+        return books.stream().mapToDouble(Book::getPrice).sum();
+    }
+    }
+
+    // OCP: New ways to select books for an order
+    interface OrderSelectionStrategy {
+    void selectBooks(Order order, Book[] availableBooks, int index);
+}
+
+
