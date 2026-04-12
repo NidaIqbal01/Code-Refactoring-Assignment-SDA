c87bb5bdc4fdf9b0ed4d137079e90b6bc50e34d3
faatimax <faatimabukhari@gmail.com>
2026-04-12 17:15:06 +0500

    ReviewService.java

---
+++ new refactored code/OCP Implementation/ReviewService.java
@@ -0,0 +1,29 @@
+package com.mycompany.bookstore;
+
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.Random;
+
+public class ReviewService {
+    private final List<String> commentBank;
+
+    public ReviewService(List<String> comments) {
+        this.commentBank = comments;
+    }
+
+    public List<Review> generateRandomReviews(Customer[] customers, Book[] books) {
+        List<Review> reviews = new ArrayList<>();
+        Random random = new Random();
+
+        for (Customer customer : customers) {
+            for (Book book : books) {
+                int rating = random.nextInt(5) + 1;
+                // Logic is now independent of the specific text
+                String comment = commentBank.get(random.nextInt(commentBank.size()));
+                reviews.add(new Review(customer, book, comment, rating));
+            }
+        }
+        return reviews;
+    }
+}
+
