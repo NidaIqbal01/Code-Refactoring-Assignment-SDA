2cb895356752d602a21033d598adff8d658acb00
faatimax <faatimabukhari@gmail.com>
2026-04-12 18:05:31 +0500

    PaymentService.java

--- new refactored code/OCP Implementation/PaymentService.java
+++ new refactored code/OCP Implementation/PaymentService.java
@@ -1,10 +1,10 @@
+
 package com.mycompany.bookstore;
 
 interface PaymentProcessor {
     boolean process(Customer customer, double amount);
 }
 
-// Concrete Implementations
 class CreditCardProcessor implements PaymentProcessor {
     public boolean process(Customer customer, double amount) {
         System.out.println("Charging Credit Card for " + customer.getUsername());
@@ -12,7 +12,16 @@ class CreditCardProcessor implements PaymentProcessor {
     }
 }
 
-// The Service (Closed for Modification)
+class PayPalProcessor implements PaymentProcessor {
+    @Override
+    public boolean process(Customer customer, double amount) {
+        System.out.println("Redirecting " + customer.getUsername() + " to PayPal gateway...");
+        System.out.println("Payment of $" + amount + " authorized via PayPal.");
+        return true;
+    }
+}
+
+
 public class PaymentService {
     public void processPayments(Customer[] customers, Book[] books, PaymentProcessor processor) {
         for (int i = 0; i < customers.length; i++) {
@@ -21,4 +30,5 @@ public class PaymentService {
         }
     }
 }
+
 
