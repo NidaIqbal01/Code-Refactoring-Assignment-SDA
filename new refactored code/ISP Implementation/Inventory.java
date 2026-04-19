5491495edf84ef057ed3203b1e9c00352b3ada51
Mahtabmazhar <mahtabmazhar1004@gmail.com>
2026-04-19 10:57:12 +0500

    Inventory.java

---
+++ new refactored code/ISP Implementation/Inventory.java
@@ -0,0 +1,22 @@
+package com.mycompany.bookstore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<Book> books;
+
+    public Inventory() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return books;
+
+    }
+
+}
