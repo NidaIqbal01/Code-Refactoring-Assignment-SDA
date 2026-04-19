3f9169be29521630c79b5d748b765b3b8c7da942
Mahtabmazhar <mahtabmazhar1004@gmail.com>
2026-04-19 11:07:14 +0500

    PricingStrategy.java

---
+++ new refactored code/ISP Implementation/PricingStrategy.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+    interface PricingStrategy {
+    double calculate(List<Book> books);
+    }
+
+    class StandardPricing implements PricingStrategy 
+    {
+        public double calculate(List<Book> books) {
+        return books.stream().mapToDouble(Book::getPrice).sum();
+    }
+    }
+
+    // OCP: New ways to select books for an order
+    interface OrderSelectionStrategy {
+    void selectBooks(Order order, Book[] availableBooks, int index);
+}
