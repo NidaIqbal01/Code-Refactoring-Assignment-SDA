6e4d6159da8bd4912e172db25572f43db99f9bcb
Mahtabmazhar <mahtabmazhar1004@gmail.com>
2026-04-19 10:55:58 +0500

    Customer.java

---
+++ new refactored code/ISP Implementation/Customer.java
@@ -0,0 +1,14 @@
+package com.mycompany.bookstore;
+
+public class Customer extends User {
+    private String email;
+
+    public Customer(String username, String password, String email) {
+        super(username, password);
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
