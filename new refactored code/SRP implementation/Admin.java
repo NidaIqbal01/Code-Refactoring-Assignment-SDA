e849d962e6507e1228b40705f6f00436b390aafe
Nida Iqbal <nidaiqbal6819@gmail.com>
2026-04-26 19:15:14 +0500

    Update Admin.java

--- new refactored code/SRP implementation/Admin.java
+++ new refactored code/SRP implementation/Admin.java
@@ -2,13 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.bookstore;
+package com.mycompany.online.bookstore.system;
 
 public class Admin extends User {
     public Admin(String username, String password) {
         super(username, password);
     }
-
+@Override
+public boolean pay(double amount) {
+    // Admins don't pay, they manage system
+    System.out.println("Admin cannot make payments.");
+    return false;
+}
     public void addBookToInventory(Book book) {
         // Logic for adding a book to the inventory
         System.out.println("Added " + book.getTitle() + " to inventory.");
