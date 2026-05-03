1744a85e31153b842d8f8764502f4787d733663d
AnfalFaruqi <anfalfrqi5647@gmail.com>
2026-05-04 01:09:41 +0500

    Update ReportDemo.java

--- new refactored code/DIP Implementation/ReportDemo.java
+++ new refactored code/DIP Implementation/ReportDemo.java
@@ -5,8 +5,6 @@
 package com.mycompany.bookstore;
 
 public class ReportDemo {
-    public static void main(String[] args) {
-
         SalesReportGenerator salesReport = new SimpleSalesReport();
         salesReport.generateSalesReport();
 
@@ -16,5 +14,4 @@ public class ReportDemo {
         inventoryReport.generateInventoryReport();
         inventoryReport.sendEmailReport();
         inventoryReport.exportToPDF();
-    }
 }
